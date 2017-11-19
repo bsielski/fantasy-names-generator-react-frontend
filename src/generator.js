2314289e771a8596640ba82acd5ddd5168b29a35
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-11-19 21:22:08 +0100

    Comment console logs

--- src/generator.js
+++ src/generator.js
@@ -14,7 +14,7 @@ export class Generator {
     this._splitters = new Set()
     this._filters = new Set()
     this._names = []
-    console.log("NAMESETS:", nameSets)
+    // console.log("NAMESETS:", nameSets)
     this._nameSets.forEach(nameSet => {
       // console.log("SPLITTES:", nameSet.splitters)
       // console.log("NAMESET:", nameSet)
