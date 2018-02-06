8820813bd63ae5de569d0f99f760414b3e61819d
Bartłomiej Sielski <b.sielski@yandex.com>
2018-02-06 14:55:17 +0100

    Extract url to .env

--- src/paths.js
+++ src/paths.js
@@ -1,4 +1,4 @@
-export const API_SERVER = "api:3001";
+export const API_SERVER = process.env.REACT_APP_BACKEND_URL;
 // export const API_SERVER = "0.0.0.0:3000";
 // export const API_SERVER = "fantasynamesgenerator.testingmagic.ovh:3001";
 // export const API_SERVER = "localhost:3001";
