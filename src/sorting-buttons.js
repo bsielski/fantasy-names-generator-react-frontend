6edd0e410e57c56e4ef317ebf4fd168d847127fb
Bartlomiej Sielski <b.sielski.webdev@gmail.com>
2019-02-06 16:38:56 +0100

    Create sorting-buttins

---
+++ src/sorting-buttons.js
@@ -0,0 +1,54 @@
+import {sortAlphabeticallyAsc} from './sorting';
+import {sortAlphabeticallyDesc} from './sorting';
+import {sortByLengthAsc} from './sorting';
+import {sortByLengthDesc} from './sorting';
+import {unsortAsc} from './sorting';
+import {unsortDesc} from './sorting';
+
+export const sortingButtons = [
+    {
+	id: 0,
+	label: "Unsorted",
+	default: "descending",
+	ascending: {
+	    alt: "from first to last generated",
+    	    icon: "002-sort-by-numeric-order.svg",
+    	    sort: unsortAsc,
+	},
+	descending: {
+	    alt: "from last to first generated",
+    	    icon: "001-sort-by-order.svg",
+    	    sort: unsortDesc,
+	}
+    },
+    {
+	id: 1,
+	label: "Alphabetically",
+	default: "ascending",
+	ascending: {
+	    alt: "from A to Z",
+    	    icon: "006-sort-by-alphabet.svg",
+    	    sort: sortAlphabeticallyAsc,
+	},
+	descending: {
+	    alt: "from Z to A",
+    	    icon: "003-sort-reverse-alphabetical-order.svg",
+    	    sort: sortAlphabeticallyDesc,
+	}
+    },
+    {
+	id: 2,
+	label: "By name length",
+	default: "ascending",
+	ascending: {
+	    alt: "from shortest to longest",
+    	    icon: "005-sort-by-attributes-interface-button-option.svg",
+    	    sort: sortByLengthAsc,
+	},
+	descending: {
+	    alt: "from longest to shortest",
+    	    icon: "004-sort-by-attributes.svg",
+    	    sort: sortByLengthDesc,
+	}
+    },
+];
