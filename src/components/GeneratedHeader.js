c7b70d3d6b88c92bf5f8185aa0ae248b7cfc5b46
Bartlomiej Sielski <b.sielski.webdev@gmail.com>
2019-02-07 20:49:15 +0100

    Use sortingButtons and pathToLastSortedMethod instead of lastSortingMethodUsed

--- src/components/GeneratedHeader.js
+++ src/components/GeneratedHeader.js
@@ -3,31 +3,21 @@ import React from 'react';
 import './GeneratedHeader.css';
 
 export class GeneratedHeader extends React.Component {
-
-  currentSorting() {
-    if (this.props.method === "unsorted") {
-      return <span className="current_sort_method">Unsorted <img className="sort_icon" alt="from first to last generated" src="002-sort-by-numeric-order.svg" width="18" height="18"/></span>;
+    render() {
+	const buttonIndex = this.props.pathToLastSortMethod[0];
+	const subbuttonName = this.props.pathToLastSortMethod[1];
+	const button = this.props.sortingButtons[buttonIndex];
+	const subbuton = button[subbuttonName];
+	const label = button.label;
+	const alt = subbuton.alt;
+	const icon = subbuton.icon;
+	return (
+	    <p className="subsection-header">
+	      <span className="current_sort_method">
+		{label}
+		<img className="sort_icon" alt={alt} src={icon} width="18" height="18"/>
+	      </span>
+	    </p>
+	);
     }
-    else if (this.props.method === "unsorted-reversed") {
-      return <span className="current_sort_method">Unsorted <img className="sort_icon" alt="from last to first generated" src="001-sort-by-order.svg" width="18" height="18"/></span>;
-    }
-    else if (this.props.method === "alphabetically") {
-      return <span className="current_sort_method">Sorted alphabetically <img className="sort_icon" alt="from A to Z" src="006-sort-by-alphabet.svg" width="18" height="18"/></span>;
-    }
-    else if (this.props.method === "alphabetically-reversed") {
-      return <span className="current_sort_method">Sorted alphabetically <img className="sort_icon" alt="from Z to A" src="003-sort-reverse-alphabetical-order.svg" width="18" height="18"/></span>;
-    }
-    else if (this.props.method === "length") {
-      return <span className="current_sort_method">Sorted by name length <img className="sort_icon" alt="from shortest to longest" src="005-sort-by-attributes-interface-button-option.svg" width="18" height="18"/></span>;
-    }
-    else if (this.props.method === "length-reversed") {
-      return <span className="current_sort_method">Sorted by name length <img className="sort_icon" alt="from longest to shortest" src="004-sort-by-attributes.svg" width="18" height="18"/></span>;
-    }
-  }
-
-  render() {
-    return (
-      <p className="subsection-header">{this.currentSorting()}</p>
-    );
-  }
 }
