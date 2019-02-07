3c2e3988129241fa52e2a0800f604cf3109f690b
Bartlomiej Sielski <b.sielski.webdev@gmail.com>
2019-02-07 19:51:04 +0100

    Generate SortingButtons from external data instead of hardcoding

--- src/components/Sorting.js
+++ src/components/Sorting.js
@@ -5,39 +5,30 @@ import './Sorting.css';
 
 export class Sorting extends React.Component {
 
-  render() {
-    return (
-      <section className="sorting">
-        <h2 className="subsection-header">Sorting methods</h2>
-        <div className="sort-buttons">
-          <SortButton label="Unsorted"
-            alt="from first to last generated"
-            altReversed="from last to first generated"
-            icon="002-sort-by-numeric-order.svg"
-            iconReversed="001-sort-by-order.svg"
-            sort={this.props.unsort}
-            startReversed={true}
-            names={this.props.generated}
-          />
-          <SortButton label="Alphabetically"
-            alt="from A to Z"
-            altReversed="from Z to A"
-            icon="006-sort-by-alphabet.svg"
-            iconReversed="003-sort-reverse-alphabetical-order.svg"
-            sort={this.props.sortAlphabetically}
-            names={this.props.generated}
-          />
-          <SortButton label="By name length"
-            alt="from shortest to longest"
-            altReversed="from longest to shortest"
-            icon="005-sort-by-attributes-interface-button-option.svg"
-            iconReversed="004-sort-by-attributes.svg"
-            sort={this.props.sortByLength}
-            names={this.props.generated}
-          />
-        </div>
-      </section>
-    )
-  }
+    render() {
+	const sortButton = (sortingButton, names, afterSorting) => {
+	    return (
+		<SortButton
+		  key={sortingButton.id}
+		  sortingButton={sortingButton}
+		  afterSorting={afterSorting}
+		  names={names}
+		  />
+	    );
+	};
+
+	const sortButtons = this.props.sortingButtons.map(sortingButton =>{
+	    return sortButton(sortingButton, this.props.generated, this.props.afterSorting);
+	});
+	
+	return (
+	    <section className="sorting">
+              <h2 className="subsection-header">Sorting methods</h2>
+              <div className="sort-buttons">
+		{sortButtons}
+              </div>
+	    </section>
+	)
+    }
 
 }
