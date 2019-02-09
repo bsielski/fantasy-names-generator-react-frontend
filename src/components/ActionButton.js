d957dc10b0bb1f61172a2599020589aa73e6d7ec
Bartlomiej Sielski <b.sielski.webdev@gmail.com>
2019-02-09 15:55:50 +0100

    Indentations

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -4,42 +4,42 @@ import './ActionButton.css';
 
 export function ActionButton(props) {
 
-  const isDisabled = () => {
-    if (props.isGenerating) {
-      return true;
+    const isDisabled = () => {
+	if (props.isGenerating) {
+	    return true;
+	}
+	else if (props.howManyNamesetsSelected === 0) {
+	    return true;
+	}
+	else {
+	    return false;
+	}
     }
-    else if (props.howManyNamesetsSelected === 0) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
 
-  const generateButtonText = () => {
-    if (props.isGenerating) {
-      return "Generating, please wait"
+    const generateButtonText = () => {
+	if (props.isGenerating) {
+	    return "Generating, please wait"
+	}
+	else if (props.howManyNamesetsSelected === 0) {
+	    return "Select some nameset or namesets"
+	}
+	else {
+	    return "Generate " + props.howManyNames + " names"
+	}
     }
-    else if (props.howManyNamesetsSelected === 0) {
-      return "Select some nameset or namesets"
-    }
-    else {
-      return "Generate " + props.howManyNames + " names"
-    }
-  }
 
-  return (
-    <section className="action_button">
-      <h2 className="action_button__header">Generate names</h2>
+    return (
+	<section className="action_button">
+	  <h2 className="action_button__header">Generate names</h2>
   	  <button
             type="button"
             id="generate-button"
             className="action_button__button"
             disabled={ isDisabled() }
             onClick={props.onClick}
-          >
-        {generateButtonText()}
-      </button>
-    </section>
-  );
+            >
+            {generateButtonText()}
+	  </button>
+	</section>
+    );
 }
