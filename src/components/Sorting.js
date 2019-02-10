e9e6eeb4fb4266d3ce5f5a144b9c6ba14d0318f0
Bartlomiej Sielski <b.sielski.webdev@gmail.com>
2019-02-10 15:35:03 +0100

    Change the class to a function

--- src/components/Sorting.js
+++ src/components/Sorting.js
@@ -3,31 +3,29 @@ import {SortButton} from './SortButton';
 
 import './Sorting.css';
 
-export class Sorting extends React.Component {
+export function Sorting(props) {
 
-    render() {
-	const sortButton = (sortingButton, names, afterSorting) => {
-	    return (
-		<SortButton
-		  key={sortingButton.id}
-		  sortingButton={sortingButton}
-		  afterSorting={afterSorting}
-		  names={names}
-		/>
-	    );
-	};
-
-	const sortButtons = this.props.sortingButtons.map(sortingButton =>{
-	    return sortButton(sortingButton, this.props.generated, this.props.afterSorting);
-	});
-	
+    const sortButton = (sortingButton, names, afterSorting) => {
 	return (
-	    <section className="sorting">
-              <h2 className="subsection-header">Sorting methods</h2>
-              <div className="sort-buttons">
-		{sortButtons}
-              </div>
-	    </section>
+	    <SortButton
+	      key={sortingButton.id}
+	      sortingButton={sortingButton}
+	      afterSorting={afterSorting}
+	      names={names}
+	    />
 	);
-    }
+    };
+
+    const sortButtons = props.sortingButtons.map(sortingButton =>{
+	return sortButton(sortingButton, props.generated, props.afterSorting);
+    });
+    
+    return (
+	<section className="sorting">
+          <h2 className="subsection-header">Sorting methods</h2>
+          <div className="sort-buttons">
+	    {sortButtons}
+          </div>
+	</section>
+    );
 }
