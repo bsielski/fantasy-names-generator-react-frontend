a52b0c00661128889502bc62794d49c5867e71be
Bartlomiej Sielski <b.sielski.webdev@gmail.com>
2019-02-20 18:42:21 +0100

    Add CustomGroupboxes as children

--- src/components/GroupboxContainer.js
+++ src/components/GroupboxContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Groupbox} from './Groupbox';
+import {CustomGroupbox} from './CustomGroupbox';
 
 import './GroupboxContainer.css';
 
@@ -31,18 +32,30 @@ export class GroupboxContainer extends React.Component {
                   subgroups={this.state.subgroups[group.id]}
                   namesets={this.state.namesets}
                   aftertToggleNamesetCheckbox={this.props.aftertToggleNamesetCheckbox}
-                  registerNameset={this.props.registerNameset}
+                />
+            );
+        };
+        const buildCustomGroupbox = (nameset, index) => {
+            return (
+                <CustomGroupbox
+                  key={index}
+                  namesetId={index.toString()}
+                  defaultNameset={nameset}
+                  aftertToggleCustomNamesetCheckbox={this.props.aftertToggleCustomNamesetCheckbox}
+                  aftertChangeCustomNamesetTextArea={this.props.aftertChangeCustomNamesetTextArea}
                 />
             );
         };
 
         const groupboxes = this.state.groups.map(buildGroupbox);
+        const customGroupboxes = this.props.customNamesetsForGenerator.map(buildCustomGroupbox);
 
         return (
             <div>
               <h2 className="subsection-header">Pick a nameset or namestes</h2>
               <section className="group-container">
                 {groupboxes}
+                {customGroupboxes}
               </section>
             </div>
         );
