48b8b7142b1a5b98dfa1d8a33bf7e0c5329f8c6d
Bartlomiej Sielski <b.sielski.webdev@gmail.com>
2019-02-20 18:40:43 +0100

    Remove all logic, leave only basic render function

--- src/components/CustomNamesArea.js
+++ src/components/CustomNamesArea.js
@@ -2,66 +2,16 @@ import React from 'react';
 
 import './CustomNamesArea.css';
 
-import {API_SERVER} from '../paths';
+export function CustomNamesArea(props) {
 
-export class CustomNamesArea extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            names: "",
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    arrayToText(array) {
-        let result = "";
-        array.forEach(variant => {
-            result += variant + "\n";
-        });
-        return result;
-    }
-
-    textToArray(text) {
-        let result = text.split("\n");
-        result = result.filter(v => v.trim() !== '');
-        return result;
-    }
-
-    handleChange(event) {
-        this.setState({names: event.target.value});
-        this.props.registerNameset(this.props.namesetId, this.textToArray(event.target.value));
-    }
-
-    componentDidMount() {
-        if (this.props.custom === true) {
-            fetch('http://' + API_SERVER + '/api/v1/names?filter[nameset-id]=' + this.props.namesetId)
-                .then(response => {
-                    return response.json();
-                })
-                .then(response => {
-                    this.setState(
-                        {names:  this.arrayToText(response.data.map(name => name.attributes.variants))},
-                    );
-                })
-                .catch(error => console.log(error));
-        }
-    }
-
-    render() {
-        if (this.props.custom === true) {
-            return (
-                <textarea
-                  name="textarea"
-                  className="custom_names__text_area"
-                  rows="10"
-                  value={this.state.names}
-                  onChange={this.handleChange}
-                >
-                </textarea>
-            );
-        }
-        else {
-            return null;
-        }
-    }
+    return (
+        <textarea
+          name="textarea"
+          className="custom_names__text_area"
+          rows="10"
+          value={props.text}
+          onChange={props.afterChangeTextArea}
+        >
+        </textarea>
+    );
 }
