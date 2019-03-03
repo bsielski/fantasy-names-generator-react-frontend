c0ac0eb52112d2cc16762a623c9d397f6f1ac247
Bartlomiej Sielski <b.sielski.webdev@gmail.com>
2019-03-03 21:12:17 +0100

    One universal RadioButton component for number or sorting

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+function Icon(props) {
+  if (!props.icon) {
+    return null;
+  }
+
+  return (
+    <img className="sort_icon" alt={props.alt} src={props.icon} width="15" height="15"/>
+  );
+}
+
 export function RadioButton(props) {
     return (
         <div>
@@ -7,6 +17,7 @@ export function RadioButton(props) {
             <input type="radio" name={props.name} value={props.value}
                    onChange={props.onChange} checked={props.checked} data-button-number={props.buttonNumber}/>
             {props.label + " " + props.description}
+            <Icon icon={props.icon} alt={props.alt}/>
           </label>
         </div>
     );
