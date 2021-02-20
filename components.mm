645f8dae693c9cf1e4c174db60614785f3e75168
Bartłomiej Sielski <b.sielski.webdev@gmail.com>
2021-02-20 20:52:49 +0100

    Make mindmap of components

---
+++ components.mm
@@ -0,0 +1,103 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node LOCALIZED_TEXT="new_mindmap" FOLDED="false" ID="ID_1871721494" CREATED="1613834221993" MODIFIED="1613834221993" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="App" POSITION="right" ID="ID_1278846551" CREATED="1613838768574" MODIFIED="1613838770407">
+<edge COLOR="#ff0000"/>
+<node TEXT="Header" ID="ID_874342696" CREATED="1613838777314" MODIFIED="1613838780019"/>
+<node TEXT="Output" ID="ID_1544559916" CREATED="1613838785682" MODIFIED="1613838789123">
+<node TEXT="ActionButton" ID="ID_1821596375" CREATED="1613838816433" MODIFIED="1613838821619">
+<node TEXT="ActionButtonText" ID="ID_338095239" CREATED="1613838996714" MODIFIED="1613839010115"/>
+</node>
+<node TEXT="GeneratedList" ID="ID_409769036" CREATED="1613838822185" MODIFIED="1613838828139">
+<node TEXT="GeneratedHeader" ID="ID_256114707" CREATED="1613839023899" MODIFIED="1613839035932"/>
+<node TEXT="GeneratedItems" ID="ID_1209631481" CREATED="1613839043346" MODIFIED="1613839051963"/>
+</node>
+<node TEXT="HowManyNames" ID="ID_1590106779" CREATED="1613838828681" MODIFIED="1613838833891"/>
+<node TEXT="SortingButtons" ID="ID_1719421532" CREATED="1613838834186" MODIFIED="1613838840971"/>
+<node TEXT="RadioButton" ID="ID_1360520145" CREATED="1613839145833" MODIFIED="1613839150964"/>
+</node>
+<node TEXT="Input" ID="ID_1328223428" CREATED="1613838782801" MODIFIED="1613838784915">
+<node TEXT="GroupboxContainer" ID="ID_100839474" CREATED="1613838851890" MODIFIED="1613838861971">
+<node TEXT="CustomGroupbox" ID="ID_1413210873" CREATED="1613838872058" MODIFIED="1613838880163">
+<node TEXT="CustomSubgroup" ID="ID_1032188107" CREATED="1613838936274" MODIFIED="1613838942228">
+<node TEXT="CustomNameset" ID="ID_1837499803" CREATED="1613838953803" MODIFIED="1613838969419">
+<node TEXT="CustomNamesArea" ID="ID_1004763579" CREATED="1613838971435" MODIFIED="1613838982187"/>
+</node>
+</node>
+</node>
+<node TEXT="Groupbox" ID="ID_1910338486" CREATED="1613838884082" MODIFIED="1613838888275">
+<node TEXT="Subgroup" ID="ID_846109728" CREATED="1613838898722" MODIFIED="1613838905053">
+<node TEXT="Nameset" ID="ID_1348428130" CREATED="1613838911066" MODIFIED="1613838915363">
+<node TEXT="Namelist" ID="ID_216411003" CREATED="1613838919612" MODIFIED="1613838923987"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Footer" ID="ID_1768169353" CREATED="1613838780297" MODIFIED="1613838782403"/>
+</node>
+</node>
+</map>
